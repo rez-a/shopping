0daa7dcf10188a0c99f9362702a5d4de99cfe764
rez-a <reza.hasani11177@gmail.com>
2024-05-12 23:01:38 +0430

    initial commit - config

---
+++ src/assets/fonts.js
@@ -0,0 +1,45 @@
+import localFont from 'next/font/local';
+
+const iranYekan = localFont({
+  src: [
+    {
+      path: '../../public/fonts/iranYekan/iranyekanwebblackfanum.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/iranYekan/iranyekanwebextraboldfanum.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/iranYekan/iranyekanwebboldfanum.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/iranYekan/iranyekanwebmediumfanum.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/iranYekan/iranyekanwebregularfanum.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/iranYekan/iranyekanweblightfanum.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/iranYekan/iranyekanwebthinfanum.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-iranyekan',
+  display: 'swap',
+});
+
+export { iranYekan };
